c6726c91cc5d7295a3d30409527b2e83df02d056
arib411 <soufian.arib@gmail.com>
2024-04-12 02:10:03 +0000

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,148 +1,140 @@
 #!/usr/bin/python3
 """
-The N queens puzzle is the challenge of placing N
-non-attacking queens on an N×N chessboard.
-
-TODO:
-    * Write a program that solves the N queens problem.
+   Description: The N queens puzzle is the challenge of placing N non-attacking
+                queens on an N×N chessboard. Write a program that solves the N
+                queens problem.
+   Usage: nqueens N:
+          If the user called the program with the wrong number of arguments,
+          print Usage: nqueens N, followed by a new line, and exit with the
+          status 1
+   where N must be an integer greater or equal to 4:
+          If N is not an integer, print N must be a number, followed by a new
+          line, and exit with the status 1
+          If N is smaller than 4, print N must be at least 4, followed by a new
+          line, and exit with the status 1
+   The program should print every possible solution to the problem:
+          One solution per line
+          Format: see example
+          You don’t have to print the solutions in a specific order
+   You are only allowed to import the sys module
 """
 
 
-def queens(chessBoard, row, majestyz, resolve):
-    """
-    Args:
-        chessBoard (list)
-        row (int)
-        majestyz (int)
-        resolve (list)
+import sys
 
+
+def print_board(board):
+    """ print_board
+    Args:
+        board - list of list with length sys.argv[1]
+    """
+    new_list = []
+    for i, row in enumerate(board):
+        value = []
+        for j, col in enumerate(row):
+            if col == 1:
+                value.append(i)
+                value.append(j)
+        new_list.append(value)
+
+    print(new_list)
+
+
+def isSafe(board, row, col, number):
+    """ isSafe
+    Args:
+        board - list of list with length sys.argv[1]
+        row - row to check if is safe doing a movement in this position
+        col - col to check if is safe doing a movement in this position
+        number: size of the board
+    Return: True of False
+    """
+
+    # Check this row in the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solveNQUtil(board, col, number):
+    """ Auxiliar method to find the posibilities of answer
+    Args:
+        board - Board to resolve
+        col - Number of col
+        number - size of the board
     Returns:
-        resolve (list)
+        All the posibilites to solve the problem
     """
-    if majestyz == len(chessBoard):
-        resolve.append(extract(chessBoard))
-        return (resolve)
 
-    for col in range(len(chessBoard)):
-        if chessBoard[row][col] == -1:
-            demo = copyBoard(chessBoard)
-            demo[row][col] = 1
-            cancel(demo, row, col)
-            resolve = queens(demo, row + 1, majestyz + 1, resolve)
-    return (resolve)
+    if (col == number):
+        print_board(board)
+        return True
+    res = False
+    for i in range(number):
+
+        if (isSafe(board, i, col, number)):
+
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Make result true if any placement
+            # is possible
+            res = solveNQUtil(board, col + 1, number) or res
+
+            board[i][col] = 0  # BACKTRACK
+
+    return res
 
 
-def cancel(chessBoard, row, col):
-    """
-    Cancels out vulnerable positions for the queen
-
+def solve(number):
+    """ Find all the posibilities if exists
     Args:
-        chessBoard (list)
-        row (int)
-        col (int)
+        number - size of the board
     """
-    length = len(chessBoard)
-    """Cancel forward positions"""
-    for c in range(col + 1, length):
-        chessBoard[row][c] = 0
-    """Cancel backwards positions"""
-    for c in range(col - 1, -1, -1):
-        chessBoard[row][c] = 0
-    """Cancel down positions"""
-    for r in range(row + 1, length):
-        chessBoard[r][col] = 0
-    """Cancel up positions"""
-    for r in range(row - 1, -1, 1):
-        chessBoard[r][col] = 0
-    """Cancel right downward diagonal positions"""
-    c = col + 1
-    for r in range(row + 1, length):
-        if c >= length:
-            break
-        chessBoard[r][c] = 0
-        c += 1
-    """Cancel left upward diagonal positions"""
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        chessBoard[r][c] = 0
-        c -= 1
-    """Cancel right upward diagonal positions"""
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= length:
-            break
-        chessBoard[r][c] = 0
-        c += 1
-    """Cancel left downward diagonal positions"""
-    c = col - 1
-    for r in range(row + 1, length):
-        if c < 0:
-            break
-        chessBoard[r][c] = 0
-        c -= 1
+    board = [[0 for i in range(number)]for i in range(number)]
+
+    if not solveNQUtil(board, 0, number):
+        return False
+
+    return True
 
 
-def chessBoard(N):
+def validate(args):
+    """ Validate the input data to verify if the size to
+        answer is posible
+    Args:
+        args - sys.argv
     """
-    Create a board of size N * N
-    """
-    chessBoard = []
-
-    """Create rows"""
-    for row in range(N):
-        chessBoard.append([])
-
-    """Create columns"""
-    for row in chessBoard:
-        for n in range(N):
-            row.append(-1)
-
-    return (chessBoard)
-
-
-def copyBoard(chessBoard):
-    """
-    make a copy of chessBoard
-    """
-    if type(chessBoard) == list:
-        """Recursively copy"""
-        return list(map(copyBoard, chessBoard))
-    return (chessBoard)
-
-
-def extract(chessBoard):
-    """
-    Extract the required outcome from the chess board
-    """
-    outcome = []
-    for row in range(len(chessBoard)):
-        for col in range(len(chessBoard)):
-            if chessBoard[row][col] == 1:
-                outcome.append([row, col])
-                break
-    return (outcome)
-
-
-def execute():
-    import sys
-
-    if len(sys.argv) != 2:
+    if (len(args) == 2):
+        # Validate data
+        try:
+            number = int(args[1])
+        except Exception:
+            print("N must be a number")
+            exit(1)
+        if number < 4:
+            print("N must be at least 4")
+            exit(1)
+        return number
+    else:
         print("Usage: nqueens N")
-        sys.exit(1)
-    if sys.argv[1].isnumeric() is False:
-        print("N must be a number")
-        sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    chess = chessBoard(int(sys.argv[1]))
-    resultMatrix = queens(chess, 0, 0, [])
-    for row in resultMatrix:
-        print(row)
+        exit(1)
 
 
-if __name__ == '__main__':
-    execute()
+if __name__ == "__main__":
+    """ Main method to execute the application
+    """
+
+    number = validate(sys.argv)
+    solve(number)
